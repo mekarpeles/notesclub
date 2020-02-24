3376a61d25600611505c17bb3800002744fb2fb5
Hector Perez <hecpeare@gmail.com>
2020-02-24 18:43:43 +0100

    basic exercise

--- front/src/Exercise.tsx
+++ front/src/Exercise.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 interface IProps {
 }
@@ -8,16 +9,42 @@ interface IState {
 }
 
 class Exercise extends React.Component<IProps, IState> {
-  check() {
+  constructor(props: IProps) {
+    super(props)
 
+    this.state = {
+      answer: ""
+    }
   }
+
+  check = () => {
+    this.setState({answer: "a good idea to go"})
+  }
+
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const target = event.target
+    const value = target.value
+
+    this.setState({ answer: value })
+  }
+
   public render() {
+    const answer = this.state.answer
+
     return (
       <div>
         <p>I was in favour of going to the cinema.</p>
         <p><b>IDEA</b></p>
-        <p>I thought it would be ... to the cinema.</p>
-        <button onClick={this.check}>Check</button>
+        <Form.Group className="form-inline exercise">
+          <Form.Label>I thought it would be</Form.Label>
+          <Form.Control
+            type="text"
+            value={answer}
+            name="answer"
+            onChange={this.handleChange as any} />
+          <Form.Label>to the cinema.</Form.Label>
+        </Form.Group>
+        <Button onClick={this.check}>Check</Button>
       </div>
     );
   }
