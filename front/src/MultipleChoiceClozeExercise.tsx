10019d80e91aaa5fe55b7e992513bf44c5132496
Hector Perez <hecpeare@gmail.com>
2020-02-29 16:57:44 +0100

    click on choice should fill the gap

--- front/src/MultipleChoiceClozeExercise.tsx
+++ front/src/MultipleChoiceClozeExercise.tsx
@@ -8,15 +8,29 @@ interface IProps {
 
 interface IState {
   solve: boolean
+  content: string[]
+  options: string[]
 }
 
 class KeyWordTransformationExercise extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props)
+    const text = this.props.text
+    const content = text.split(/\(\d+\)\ \.+/)
+    const options = Array(content.length - 1).fill(".......")
 
     this.state = {
       solve: false,
+      content: content,
+      options: options
     }
+  }
+
+  selectChoice = (index: number, ind: number) => {
+    const { options } = this.state
+    const { choices }= this.props
+    options[index] = choices[index][ind]
+    this.setState({ options: options })
   }
 
   renderChoices = () => {
@@ -25,23 +39,36 @@ class KeyWordTransformationExercise extends React.Component<IProps, IState> {
       <div className="choices">
         <table className="multiple-choice-choices">
           {choices.map((options, index) => {
-            return (<tr>{index}:&nbsp;{options.map((word) => {
-              return (<td>{word}</td>)
+            return (<tr>{index}:&nbsp;{options.map((word, ind) => {
+              return (<td onClick={() => this.selectChoice(index, ind)}>{word}</td>)
             })}</tr>)
           })}
         </table>
       </div>
     );
   }
+  renderOption = (index: number) => {
+    const { options } = this.state
+
+    return(
+      <b>({index}) {options[index]}</b>
+    )
+  }
   public render() {
-    const { text, choices } = this.props
+    const { content } = this.state
+    const content_length = content.length
+
     return (
       <div className="exercise container">
         <div className="row">
           <div className="col-lg-3"></div>
           <div className="col-lg-6">
             <div className="multiple-choice-text">
-              {text}
+              {content.map((piece, index) => {
+                return(
+                  <>{piece}{index < content_length - 1 ? this.renderOption(index) : <></>}</>
+                )
+              })}
             </div>
 
             {this.renderChoices()}
