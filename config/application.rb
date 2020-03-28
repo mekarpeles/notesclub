eb7b61383846e1f03b4e1b2dd432ff9a0a96e000
Hector Perez <hecpeare@gmail.com>
2020-03-28 16:36:03 +0100

    rm credentials: true from cookies middleware

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,6 @@ module Arbol
       allow do
         origins 'http://hec.localhost.hec:3001', 'https://www.treeconf.com'
         resource '*',
-          credentials: true,
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
