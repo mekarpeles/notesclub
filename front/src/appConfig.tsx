3901c1b66d49964b75ef600fe7a1ba93f707fc94
Hector Perez <hecpeare@gmail.com>
2020-03-28 11:13:56 +0100

    use === instead of == in appConfig

--- front/src/appConfig.tsx
+++ front/src/appConfig.tsx
@@ -1,7 +1,7 @@
 export function apiDomain() {
-  if (process.env["NODE_ENV"] == "production") {
+  if (process.env["NODE_ENV"] === "production") {
     return ("https://api.treeconf.com")
-  } else if (process.env["NODE_ENV"] == "development") {
+  } else if (process.env["NODE_ENV"] === "development") {
     return ("http://localhost:3000")
   }
 }
