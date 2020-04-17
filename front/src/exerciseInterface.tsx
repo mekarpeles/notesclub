4881ec098dc485dacfa6663def9ad54a54561701
Hector Perez <hecpeare@gmail.com>
2020-04-17 14:34:03 +0200

    fix exerciseInterface after rm key word transf

--- front/src/exerciseInterface.tsx
+++ front/src/exerciseInterface.tsx
@@ -1,10 +1,9 @@
-import keyWordTransformationInterface from './keyWordTransformationInterface'
 import openClozeInterface from './openClozeInterface'
 
 export default interface exerciseInterface {
   id: number
   name: string
-  data: openClozeInterface | keyWordTransformationInterface
+  data: openClozeInterface
   createdAt: string
   createdById: number
 }
