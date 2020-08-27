3698831f025b2090f7a0e80adb35367612d09f27
Hector Perez <hecpeare@gmail.com>
2020-08-27 19:51:59 +0200

    turn App css into scss

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import './App.css';
+import './App.scss';
 import '@ionic/react/css/core.css';
 import Login from './Login';
 import Header from './Header';
---
+++ front/src/UserPage.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import { User, Users } from './User'
+import { Button } from 'react-bootstrap'
+
+interface UserPageProps {
+  currentUsername: string
+  users: Users<User>
+}
+
+interface UserPageState {
+
+}
+
+class UserPage extends React.Component<UserPageProps, UserPageState> {
+  public render () {
+    const { currentUsername, users } = this.props
+    const currentUser = users[currentUsername]
+
+    return (
+      <div>
+        <h1>{currentUser.name}'s topics</h1>
+        <Button variant="link" href={`/${currentUsername}/2020-07-30`}>2020-07-30</Button>
+      </div>
+    )
+  }
+}
+
+export default UserPage
