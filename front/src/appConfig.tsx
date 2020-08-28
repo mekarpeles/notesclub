ce5f6ab5c1eb31a5241c34d7d3c83e7bae1eea64
Hector Perez <hecpeare@gmail.com>
2020-08-28 20:24:56 +0200

    replace treeconf.com with wikir.org

--- front/src/appConfig.tsx
+++ front/src/appConfig.tsx
@@ -1,6 +1,6 @@
 export function apiDomain() {
   if (process.env["NODE_ENV"] === "production") {
-    return ("https://api.treeconf.com")
+    return ("https://api.wikir.org")
   } else if (process.env["NODE_ENV"] === "development") {
     return ("http://localhost:3000")
   }
