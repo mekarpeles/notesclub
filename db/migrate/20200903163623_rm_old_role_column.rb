16f8dcf20e2b026859a7dc706ee30e13e17794cf
Hector Perez <hecpeare@gmail.com>
2020-09-03 18:41:19 +0200

    rm old role column

---
+++ db/migrate/20200903163623_rm_old_role_column.rb
@@ -0,0 +1,5 @@
+class RmOldRoleColumn < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :users, :role
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_102209) do
+ActiveRecord::Schema.define(version: 2020_09_03_163623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 2020_08_30_102209) do
     t.string "name"
     t.string "username"
     t.string "jwt_token"
-    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
