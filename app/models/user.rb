8c7b79934b174166f5775f94c49fbcff54f5a870
Hector Perez <hecpeare@gmail.com>
2020-09-05 15:41:54 +0200

    skip some devise routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :confirmable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+  devise :database_authenticatable, :confirmable, :validatable
 
   self.skip_session_storage = [:http_auth, :params_auth]
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   scope :v1, defaults: { format: :json} do
     resources :users, only: [:index, :show, :create, :update]
     devise_for :users, controllers: { sessions: :sessions },
-                       path_names: { sign_in: :login, sign_out: :logout }
+                       path_names: { sign_in: :login, sign_out: :logout },
+                       skip: [:confirmations], skip_helpers: [:confirmations]
     resources :topics, only: [:index, :show, :create, :update, :destroy]
     resources :waiting_users, only: [:create]
     get 'golden_tickets/check', to: 'golden_tickets#check'
