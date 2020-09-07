b89d416218297a85a340bb360fcdac70e1af54fc
Hector Perez <hecpeare@gmail.com>
2020-09-07 18:20:58 +0200

    improve search count

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,17 @@
 class SearchController < ApplicationController
   def count
     if params['url'].present?
-      topics = Topic.where("lower(content) like ?", "%#{params['url'].downcase}%")
-      count = topics.count
+      url = params['url'].downcase
+      # We count all non-root topics (ancestry != nil):
+      count1 = Topic.
+        where("lower(content) like ?", "%#{url}%").
+        where("ancestry is not null").limit(10).count
+      # We also count root topics with a first child where content is not empty:
+      count2 = Topic.
+        joins("inner join topics as t on t.ancestry = cast(topics.id as VARCHAR(255)) and t.position=1 and t.content != ''").
+        where("topics.ancestry is null").
+        where("lower(topics.content) like ?", "%#{url}%").limit(10).count
+      count = [count1 + count2, 10].min
     else
       count = 0
     end
