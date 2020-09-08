5677632ad492b93cfd36b92e36585b8f8607855f
Hector Perez <hecpeare@gmail.com>
2020-09-08 15:27:52 +0200

    set up sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,5 +103,15 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { :host => "englishconf.com" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+          :address        => 'smtp.sendgrid.net',
+          :port           => '587',
+          :authentication => :plain,
+          :user_name      => ENV['SENDGRID_USERNAME'],
+          :password       => ENV['SENDGRID_PASSWORD'],
+          :domain         => 'heroku.com'
+  }
+  config.action_mailer.default_url_options = { :host => 'wikir.org' }
 end
