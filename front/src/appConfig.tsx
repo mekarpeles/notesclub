08f6bc6231896bc34de6bf8e369b0ea129d20544
Hector Perez <hecpeare@gmail.com>
2020-09-25 15:57:45 +0200

    setup notesclub ngrok

--- front/src/appConfig.tsx
+++ front/src/appConfig.tsx
@@ -2,6 +2,6 @@ export function apiDomain() {
   if (process.env["NODE_ENV"] === "production") {
     return ("https://api.notes.club")
   } else if (process.env["NODE_ENV"] === "development") {
-    return ("http://wikir-api.hec.ngrok.io")
+    return ("http://notesclub-api.hec.ngrok.io")
   }
 }
