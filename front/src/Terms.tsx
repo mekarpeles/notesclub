e5d02b1d4c02880069098c35689d5db6df465c40
Hector Perez <hecpeare@gmail.com>
2020-09-25 18:08:47 +0200

    include release notes in Terms

--- front/src/Terms.tsx
+++ front/src/Terms.tsx
@@ -21,13 +21,13 @@ class Terms extends React.Component<TermsProps, TermsState> {
     return (
       <div className="container">
         <h1>Terms and Conditions ("Terms")</h1>
-        <p>Last updated: 24/9/2020</p>
+        <p>Last updated: 25/9/2020</p>
         <p>Please read these Terms and Conditions carefully before using the https://book.notes.club website</p>
         <p>Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service. By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service.</p>
 
         <p><b>Content</b></p>
         <p>Our Service allows you to post, link, store, share and otherwise make available certain information, text, graphics, videos, or other material ("Content"). You are responsible for the legality of them.</p>
-        <p>At the moment all the content you store on Book Notes Club can be accessed by any other Book Notes Club user.</p>
+        <p>All the content you write on Book Notes Club can be accessed by any other Book Notes Club member. Also, every month we'll <b>release all notes</b> with a <b>Creative Commons</b> or public domain license.</p>
 
         <p><b>Links To Other Web Sites</b></p>
         <p>Our Service may contain links to third­party web sites or services that are not owned or controlled by Book Notes Club.</p>
