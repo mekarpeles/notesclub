d093821e535fcf1371d10ed74be58ed21246ae2f
Hector Perez <hecpeare@gmail.com>
2020-10-02 14:55:43 +0200

    mv specs to tests directory

--- front/src/tests/App.test.tsx
+++ front/src/tests/App.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import App from './App'
+import App from './../App'
 
-describe('Header', () => {
+describe('App', () => {
   test('renders Book Notes Club', () => {
     const { getByText } = render(<App />)
-    const linkElement = getByText(/Are you a book reader?/i)
+    const linkElement = getByText(/Book Notes Club/i)
     expect(linkElement).toBeInTheDocument()
-  });
+    expect(linkElement.getAttribute("href")).toEqual("/")
+  })
 })
